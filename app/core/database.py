2b116142f39c2b6060292df25d584cca367bd6e6
selnem <tjdghks0130@naver.com>
2026-04-08 02:19:52 +0900

    WIP: database setting

--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,9 @@
-from contextlib import contextmanager
 from typing import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-from pgvector.sqlalchemy import register_vector
 
 from app.core.config import settings
 
@@ -18,8 +15,6 @@ engine = create_engine(
     settings.sync_database_url,
     pool_pre_ping=True,
 )
-
-register_vector(engine)
 
 
 SessionLocal = sessionmaker(
